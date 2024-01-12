d143b4a7ad2ca3acbfe23bc87eac3d319ba3d360
= <nicekitae@naver.com>
2024-01-12 13:56:32 +0900

    server,client port change

--- src/AccelerationDisplay.js
+++ src/AccelerationDisplay.js
@@ -18,7 +18,7 @@ const AccelerationDisplay = () => {
 
   const fetchDataFromServer = async () => {
     try {
-      const response = await fetch('http://192.168.0.36:4001/getdata');
+      const response = await fetch('http://192.168.0.36:5001/getdata');
       const data = await response.json();
       setReceivedData(data);
       console.log('데이터를 성공적으로 받았습니다:', data);
