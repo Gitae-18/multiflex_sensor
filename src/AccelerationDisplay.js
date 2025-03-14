47edebfb8629e69d5d20f68ec1ee4032f2a4be01
= <nicekitae@naver.com>
2025-03-14 15:29:20 +0900

    update;

--- src/AccelerationDisplay.js
+++ src/AccelerationDisplay.js
@@ -325,27 +325,27 @@ const getPMBackgroundColor = (value, type) => {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr style={{ borderBottom: '1px solid #ccc' }}>
-                            <td style={{ padding: '10px', fontWeight: 'bold', borderRight: '1px solid #ccc' }}>좋음</td>
-                            <td style={{ backgroundColor: '#D4EECF', padding: '10px', borderRight: '1px solid #ccc !important' }}>0 ~ 30</td>
-                            <td style={{ backgroundColor: '#D7E9F7', padding: '10px' }}>0 ~ 15</td>
-                        </tr>
-                        <tr style={{ borderBottom: '1px solid #ccc' }}>
-                            <td style={{ padding: '10px', fontWeight: 'bold', borderRight: '1px solid #ccc'}}>보통</td>
-                            <td style={{ backgroundColor: '#FFF5CC', padding: '10px', borderRight: '1px solid #ccc !important' }}>31 ~ 80</td>
-                            <td style={{ backgroundColor: '#FCE8D5', padding: '10px' }}>16 ~ 35</td>
-                        </tr>
-                        <tr style={{ borderBottom: '1px solid #ccc' }}>
-                            <td style={{ padding: '10px', fontWeight: 'bold',borderRight: '1px solid #ccc' }}>나쁨</td>
-                            <td style={{ backgroundColor: '#FFDDC1', padding: '10px', borderRight: '1px solid #ccc' }}>81 ~ 150</td>
-                            <td style={{ backgroundColor: '#F4CCCC', padding: '10px' }}>36 ~ 75</td>
-                        </tr>
-                        <tr>
-                            <td style={{ padding: '10px', fontWeight: 'bold', borderRight: '1px solid #ccc' }}>매우나쁨</td>
-                            <td style={{ backgroundColor: '#FFB3B3', padding: '10px', borderRight: '1px solid #ccc' }}>151 이상</td>
-                            <td style={{ backgroundColor: '#E6A8A8', padding: '10px' }}>76 이상</td>
-                        </tr>
-                    </tbody>
+                      <tr style={{ borderBottom: '1px solid #ccc' }}>
+                          <td style={{ padding: '10px', fontWeight: 'bold', borderRight: '1px solid #ccc' }}>좋음</td>
+                          <td style={{ backgroundColor: '#D4EECF', padding: '10px', borderRight: '1px solid #ccc' }}>0 ~ 30</td>
+                          <td style={{ backgroundColor: '#D7E9F7', padding: '10px' }}>0 ~ 15</td>
+                      </tr>
+                      <tr style={{ borderBottom: '1px solid #ccc' }}>
+                          <td style={{ padding: '10px', fontWeight: 'bold', borderRight: '1px solid #ccc' }}>보통</td>
+                          <td style={{ backgroundColor: '#FFF5CC', padding: '10px', borderRight: '1px solid #ccc' }}>31 ~ 80</td>
+                          <td style={{ backgroundColor: '#FCE8D5', padding: '10px' }}>16 ~ 35</td>
+                      </tr>
+                      <tr style={{ borderBottom: '1px solid #ccc' }}>
+                          <td style={{ padding: '10px', fontWeight: 'bold', borderRight: '1px solid #ccc' }}>나쁨</td>
+                          <td style={{ backgroundColor: '#FFDDC1', padding: '10px', borderRight: '1px solid #ccc' }}>81 ~ 150</td>
+                          <td style={{ backgroundColor: '#F4CCCC', padding: '10px' }}>36 ~ 75</td>
+                      </tr>
+                      <tr>
+                          <td style={{ padding: '10px', fontWeight: 'bold', borderRight: '1px solid #ccc' }}>매우나쁨</td>
+                          <td style={{ backgroundColor: '#FF0000', color: 'white', fontWeight: 'bold', padding: '10px', borderRight: '1px solid #ccc' }}>151 이상</td>
+                          <td style={{ backgroundColor: '#FF0000', color: 'white', fontWeight: 'bold', padding: '10px' }}>76 이상</td>
+                      </tr>
+                  </tbody>
                 </table>
 
                 {/* 오존 및 이산화질소 기준표 */}
